dc8c80df5a2ed475b9790d6c31d4836862947242
abdallamfathy <abdallamfathy@gmail.com>
2022-08-25 12:55:14 +0200

    Loop over spans and add event listener

--- main.js
+++ main.js
@@ -3,3 +3,27 @@
 let allSpans = document.querySelectorAll(".buttons span");
 let results = document.querySelector(".results > span");
 let input = document.getElementById("the-input");
+
+// Loop over spans
+allSpans.forEach(span =>{
+    // Add event on click on span
+    span.addEventListener("click" , (e) =>{
+        // check condition
+        if (e.target.classList.contains("check-item")) {
+            checkItem();
+        }
+        // add condition
+        if (e.target.classList.contains("add-item")) {
+            addItem();
+        }
+        // delete condition
+        if (e.target.classList.contains("delete-item")) {
+            deleteItem();
+        }
+        // show condition
+        if (e.target.classList.contains("show-item")) {
+            showItem();
+        }
+        
+    })
+})
