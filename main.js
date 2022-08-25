891a7b25690accc74453fe3b8efbaf7819c8c74d
abdallamfathy <abdallamfathy@gmail.com>
2022-08-25 12:47:01 +0200

    select elements

---
+++ main.js
@@ -0,0 +1,5 @@
+// select Elements
+
+let allSpans = document.querySelectorAll(".buttons span");
+let results = document.querySelector(".results > span");
+let input = document.getElementById("the-input");
